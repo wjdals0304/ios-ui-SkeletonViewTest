f20b920990b9d3fbcdfb8208b87dc984437f4590
jmkim <wjdals0304@gmail.com>
2022-05-19 16:00:51 +0900

    Initial Commit

---
+++ SkeletonViewTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkeletonViewTest
+//
+//  Created by 김정민 on 2022/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
